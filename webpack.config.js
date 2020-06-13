cb80cf5b27251b0e13a37ae99fb1ce133ff4996b
Svetlana Antipova <die-chimaere@yandex.ru>
2020-06-13 12:40:44 +0300

    fix

--- webpack.config.js
+++ webpack.config.js
@@ -7,8 +7,6 @@ module.exports = {
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
-    publicPath: '/',
-    filename: '[name].[chunkhash].js'
   },
   module: {
     rules: [
