7ba8af67f1dda07c4696d47513bd3c24fc5361c6
Svetlana Antipova <die-chimaere@yandex.ru>
2020-06-13 12:32:01 +0300

    fix: trying to deploy

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,8 @@ module.exports = {
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
-    publicPath: "/webpack-demo/",
+    publicPath: '/',
+    filename: '[name].[chunkhash].js'
   },
   module: {
     rules: [
