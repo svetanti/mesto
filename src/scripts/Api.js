c655d08777c8f815cb05d21899b6951e189ad747
Svetlana Antipova <die-chimaere@yandex.ru>
2020-06-23 15:26:39 +0300

    feat: add counter for card likes

---
+++ src/scripts/Api.js
@@ -0,0 +1,20 @@
+class Api {
+  constructor(options) {
+    this._url = options.baseUrl;
+    this._headers = options.headers;
+  }
+
+  getInitialCards() {
+    // ...
+  }
+
+  // другие методы работы с API
+}
+
+const api = new Api({
+  baseUrl: 'https://mesto.nomoreparties.co/v1/cohort-42',
+  headers: {
+    authorization: 'c56e30dc-2883-4270-a59e-b2f7bae969c6',
+    'Content-Type': 'application/json'
+  }
+});
